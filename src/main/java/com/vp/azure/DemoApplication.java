1a83a93d06bf171b2e205e1d535bb78023fc28d4
Vaibhav Parkhi <parkhi.vaibhav@gmail.com>
2025-02-06 16:22:58 +0530

    updated status code

--- src/main/java/com/vp/azure/DemoApplication.java
+++ src/main/java/com/vp/azure/DemoApplication.java
@@ -2,8 +2,13 @@ package com.vp.azure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @SpringBootApplication
 @RestController
@@ -13,9 +18,14 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/message")
-	public String message() {
-		return "App deploy successfully on Azure!";
+	@GetMapping("/message/{id}")
+	public ResponseEntity message(@PathVariable String id) {
+
+		if("200".equals(id)) {
+			return new ResponseEntity<>("App deploy successfully on Azure!", HttpStatus.OK);
+		} else {
+			return new ResponseEntity("Not found!", HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
